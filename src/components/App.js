d03848f2b82c587a1a0165c605756a1ab2846789
Abhishek Rajput <98444175+abhishekrajput6916@users.noreply.github.com>
2023-10-08 13:19:01 +0000

    update app

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,36 @@
 import React, { useReducer } from 'react'
 import { counterReducer } from '../reducers/counterReducer';
 import '../styles/App.css';
-import ControlBtn from './ControlBtn';
+// import ControlBtn from './ControlBtn';
 const App = () => {
 const [state,dispatch] = useReducer(counterReducer,{count:0})
   return (
-    <div id="main">
-      <span id="counter">{state.count}</span>
-      <ControlBtn id="increment-btn" dispatch={dispatch} state={state} payload="Inc"/>
-      <ControlBtn id="decrement-btn" dispatch={dispatch} state={state} payload="Dec"/>
+    <div className="App">
+      <h1>
+        Count:{state.count}
+      </h1>
+      <button
+      id="increment-btn"
+        onClick={() => {
+          dispatch({ type: "Inc" });
+        }}
+      >
+        Inc
+      </button>
+      <button
+      id="decrement-btn"
+        onClick={() => {
+          dispatch({ type: "Dec" });
+        }}
+      >
+        Dec
+      </button>
     </div>
+    // <div id="main">
+    //   <span id="counter">{state.count}</span>
+    //   <ControlBtn id="increment-btn" dispatch={dispatch} state={state} payload="Inc"/>
+    //   <ControlBtn id="decrement-btn" dispatch={dispatch} state={state} payload="Dec"/>
+    // </div>
   )
 }
 
